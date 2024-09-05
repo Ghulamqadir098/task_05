6c5b7ac9e2bd2f5fea005f36bcefd593634fe660
Ghulam Qadir <idontknow131422@gmail.com>
2024-09-05 23:16:26 +0500

    Second commit

---
+++ resources/views/pages/categories/category_form.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.layout')
+
+
+@section('content')
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header bg-primary text-white">Add a Category</div>
+                <div class="card-body">
+                    <form method="POST" action="#">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="category" class="form-label">Category</label>
+                            <input type="text" class="form-control" id="email" name="name" required autofocus>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Create</button>
+                    
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
